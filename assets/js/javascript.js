8dbcf6111f40b7ab0b48efac39206eb5308dfd0e
BastianPerezG <bastianesteban.perez@gmail.com>
2023-04-19 11:46:47 -0400

    probando3

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+const users = {
+    'alice': 'password123',
+    'bob': 'secret321'
+};
+
+const form = document.querySelector('#login-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    const username = document.querySelector('#username-input').value;
+    const password = document.querySelector('#password-input').value;
+    // Aquí es donde vamos a manejar la verificación de credenciales y redirigir al usuario
+
+if (username === 'user' &&  password === 'hola.123') {
+    // Las credenciales son correctas, redirigir al usuario a la página principal
+    location.href = '../../index.html';
+} else {
+    // Las credenciales son incorrectas, limpiar el formulario y permitir un nuevo intento
+    form.reset();
+}
+});
